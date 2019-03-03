ccba2d5d34c469ea4d6a9f38ed51dcbc744d8229
Patrick Qu <quteng.bo@gmail.com>
2019-03-03 16:52:30 -0500

    aknowledgement added

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,13 +1,58 @@
 const socket = io();
 
+// Elements
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
+
 socket.on('message', (message) => {
   console.log(message);
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+socket.on('location', (locationInfo) => {
+  console.log(locationInfo)
+})
+
+$messageForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  const message = document.querySelector('input').value;
+  $messageFormButton.setAttribute('disabled', 'disabled');
+  // disable
 
-  socket.emit('sendMessage', message);
+  const message = e.target.elements.message.value;
+
+  socket.emit('sendMessage', message, (error) => {
+    $messageFormButton.removeAttribute('disabled');
+    $messageFormInput.value = '';
+    $messageFormInput.focus();
+    //enable
+    
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log('The message was delivered', message);
+  });
 });
+
+$sendLocationButton.addEventListener('click', () => {
+  if(!navigator.geolocation) {
+    return alert('location not supported by your browser')
+  }
+
+  $sendLocationButton.setAttribute('disabled', 'disabled');
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    // console.log(position);
+    const locationInfo = {
+      lat: position.coords.latitude,
+      long: position.coords.longitude
+    }
+
+    socket.emit('sendLocation', locationInfo, () => {
+      console.log('Location shared!');
+      $sendLocationButton.removeAttribute('disabled');
+    })
+  })
+})
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const Filter = require('bad-words');
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -16,12 +17,36 @@ const welcomeMessage = 'Welcome to the chat app';
 io.on('connection', (socket) => {
   console.log('New webSocket connection');
 
-  socket.on('sendMessage', (message) => {
-    console.log(message);
-    io.emit('message', message);
+  socket.emit('welcome', welcomeMessage);
+  socket.broadcast.emit('message', 'A new has joined!')
+
+  socket.on('showMessage', () => {
+    io.emit('welcome', welcomeMessage);
   });
 
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter();
+
+    if(filter.isProfane(message)) {
+      return callback('Profanity is not allowed!');
+    }
+      
+    io.emit('message', message);
+    callback('Delivered!');
+  });
+
+  socket.on('sendLocation', (locationInfo, callback) => {
+    const locationToString = `https://www.google.com/maps?q=${locationInfo.lat},${locationInfo.long}`
+    io.emit('location', locationToString);
+    callback();
+  })
+  
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A new has left!');
+  })
 });
+
 
 server.listen(port, () => {
   console.log(`server is up on port ${port}!`);
